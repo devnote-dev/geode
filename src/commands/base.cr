07564533d61010a4ef521b1735ed997edb5407e6
devnote-dev <inferno0745@gmail.com>
2023-01-28 00:13:26 +0000

    feat(commands): implement base command class

---
+++ src/commands/base.cr
@@ -0,0 +1,58 @@
+module Geode::Commands
+  abstract class BaseCommand < Cling::Command
+    def initialize
+      super
+
+      @inherit_options = true
+      add_option "no-color", description: "disable ansi color formatting"
+      add_option 'h', "help", description: "get help information"
+    end
+
+    def help_template : String
+      <<-TEXT
+      #{"❖  Geode".colorize.magenta}: #{"A Crystal Build Tool".colorize.light_magenta}
+
+      #{"Options".colorize.magenta}
+      »  -h, --help  sends help information
+      TEXT
+    end
+
+    def pre_run(arguments : Cling::Arguments, options : Cling::Options) : Bool
+      Colorize.enabled = false if options.has? "no-color"
+
+      if options.has? "help"
+        stdout.puts help_template
+
+        false
+      else
+        true
+      end
+    end
+
+    def on_unknown_arguments(args : Array(String))
+      stderr.puts %(#{"❖".colorize.red}  Unexpected argument#{"s" if args.size > 1} for this command:)
+      if args.size > 1
+        stderr.puts %(#{"»".colorize.red}  #{args[..-2].join ", "} and #{args.last})
+      else
+        stderr.puts %(#{"»".colorize.red}  #{args.first})
+      end
+
+      command = %(geode #{self.name == "app" ? "" : self.name + " "}--help).colorize.light_magenta
+      stderr.puts "\nSee '#{command}' for more information"
+      exit 1
+    end
+
+    def on_unknown_options(options : Array(String))
+      stderr.puts %(#{"❖".colorize.red}  Unexpected option#{"s" if options.size > 1} for this command:)
+      if options.size > 1
+        stderr.puts %(#{"»".colorize.red}  #{options[..-2].join ", "} and #{options.last})
+      else
+        stderr.puts %(#{"»".colorize.red}  #{options.first})
+      end
+
+      command = %(geode #{self.name == "app" ? "" : self.name + " "}--help).colorize.light_magenta
+      stderr.puts "\nSee '#{command}' for more information"
+      exit 1
+    end
+  end
+end
--- src/geode.cr
+++ src/geode.cr
@@ -2,6 +2,7 @@ require "cling"
 require "colorize"
 require "yaml"
 
+require "./commands/*"
 require "./package"
 
 Colorize.on_tty_only!
@@ -9,18 +10,9 @@ Colorize.on_tty_only!
 module Geode
   VERSION = "0.1.0"
 
-  class CLI < Cling::Command
+  class CLI < Commands::BaseCommand
     def setup : Nil
       @name = "app"
-    end
-
-    def help_template : String
-      <<-TEXT
-      #{"Geode".colorize.magenta} ❖  #{"A Crystal Build Tool".colorize.light_magenta}
-
-      #{"Options".colorize.magenta}
-      »  -h, --help  sends help information
-      TEXT
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
