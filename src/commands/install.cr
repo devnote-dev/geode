60ecc5f4f16bbdf6fc9d5cf9795ce8a62af63fc7
devnote-dev <devnote.dev75@gmail.com>
2023-06-17 05:15:37 +0100

    feat(*): code cleanup and linting

--- src/commands/install.cr
+++ src/commands/install.cr
@@ -4,7 +4,7 @@ module Geode::Commands
       @name = "install"
       @summary = "installs dependencies from shard.yml"
       @description = "Installs dependencies from a shard.yml file. This includes development dependencies " \
-        "unless you include the '--production' flag."
+                     "unless you include the '--production' flag."
 
       add_usage "install [-D|--no-development] [--frozen] [--production] [--verbose]"
       add_option 'D', "no-development"
@@ -18,24 +18,22 @@ module Geode::Commands
       nodev = options.has? "no-development"
       frozen = options.has? "frozen"
       production = options.has? "production"
+      return unless (nodev || frozen) && production
 
-      if (nodev || frozen) && production
-        flags = [] of String
-        flags << "no-development" if nodev
-        flags << "frozen" if frozen
+      flags = [] of String
+      flags << "no-development" if nodev
+      flags << "frozen" if frozen
 
-        stdout.puts "#{"❖  Warning".colorize.yellow}: unnecessary flag#{"s" if nodev && frozen} specified:"
-        stdout.puts "#{"»".colorize.yellow}  production"
-        stdout.puts %(#{"»".colorize.yellow}    ↳ implies #{flags.join " and "})
-      end
+      stdout.puts "#{"❖  Warning".colorize.yellow}: unnecessary flag#{"s" if nodev && frozen} specified:"
+      stdout.puts "#{"»".colorize.yellow}  production"
+      stdout.puts %(#{"»".colorize.yellow}    ↳ implies #{flags.join " and "})
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
-      unless File.exists? "shard.yml"
-        stderr.puts "#{"❖ Error".colorize.red}: shard.yml file not found"
-        stderr.puts "#{"»".colorize.red}  Run '#{"geode init".colorize.light_magenta}' to create one"
-        system_exit
-      end
+      return if File.exists? "shard.yml"
+      stderr.puts "#{"❖ Error".colorize.red}: shard.yml file not found"
+      stderr.puts "#{"»".colorize.red}  Run '#{"geode init".colorize.light_magenta}' to create one"
+      system_exit
     end
   end
 end
--- src/geode.cr
+++ src/geode.cr
@@ -14,11 +14,22 @@ module Geode
   class CLI < Commands::BaseCommand
     def setup : Nil
       @name = "app"
-      @header = %(#{"❖  Geode".colorize.magenta}: #{"A Crystal Build Tool".colorize.light_magenta})
+      @header = %(#{"❖  Geode".colorize.magenta}: #{"A Crystal Package Manager".colorize.light_magenta})
 
       add_command Commands::Version.new
       add_command Commands::Init.new
+      # add_command Commands::New.new
       add_command Commands::Install.new
+      # add_command Commands::Add.new
+      # add_command Commands::Check.new
+      # add_command Commands::Update.new
+      # add_command Commands::Build.new
+      # add_command Commands::Vendor.new
+      # add_command Commands::Remove.new
+      # add_command Commands::List.new
+      # add_command Commands::Info.new
+      # add_command Commands::Run.new
+      # add_command Commands::Config.new
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
