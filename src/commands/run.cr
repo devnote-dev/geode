ceef9b79bae5411a2c62dc1d730d54e42766f0d5
devnote-dev <devnote.dev75@gmail.com>
2023-10-25 23:14:12 +0100

    feat(commands): support targeted shards for run script

--- src/commands/run.cr
+++ src/commands/run.cr
@@ -4,12 +4,13 @@ module Geode::Commands
       @name = "run"
       @summary = "runs a script from shard.yml"
       @description = <<-DESC
-        Runs a specified script from a shard.yml file. If target platforms are set for
-        the script, the triple matching the host system will be used. This can be
-        overriden by specifying the '--target' flag. There are also special 'linux' and
-        'windows' group targets that can be used to target Linux-based systems and
-        Windows systems respectively. If there is no matching target triple for the host
-        system and no override, the command will fail.
+        Runs a specified script from a local shard.yml file. You can also run a script
+        from an installed shard by specifying the '--shard' flag. If target platforms
+        are set for the script, the triple matching the host system will be used. This
+        can be overriden by specifying the '--target' flag. There are also special
+        'linux' and 'windows' group targets that can be used to target Linux-based
+        systems and Windows systems respectively. If there is no matching target triple
+        for the host system and no override, the command will fail.
 
         The host triple for this platform is: #{Geode::HOST_TRIPLE}
 
@@ -41,13 +42,44 @@ module Geode::Commands
         • x86_64-pc-windows-msvc
         DESC
 
-      add_usage "run [--target <name>] <script>"
+      add_usage "run [--shard <name>] [--target <name>] <script>"
       add_argument "script", description: "the name of the script", required: true
+      add_option "shard", description: "the name of the shard", type: :single
       add_option "target", description: "the target triple", type: :single
     end
 
     def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
       shard = Shard.load_local
+      dir = Dir.current
+
+      if options.has? "shard"
+        name = options.get("shard").as_s
+
+        if shard.dependencies.has_key? name
+          if File.exists?(path = Path["lib", name, "shard.yml"])
+            shard = Shard.from_yaml File.read path
+            dir = path.dirname
+          else
+            error "Shard '#{name}' is listed as a dependency but not installed"
+            system_exit
+          end
+        elsif shard.development.has_key? name
+          if File.exists?(path = Path["lib", name, "shard.yml"])
+            shard = Shard.from_yaml File.read path
+            dir = path.dirname
+          else
+            error "Shard '#{name}' is listed as a development dependency but not installed"
+            system_exit
+          end
+        elsif File.exists? Path["lib", name, "shard.yml"]
+          error "Shard '#{name}' is installed but not listed as a dependency"
+          system_exit
+        else
+          error "Shard '#{name}' is not installed"
+          system_exit
+        end
+      end
+
       if shard.scripts.empty?
         error "No scripts defined in shard.yml"
         system_exit
@@ -71,10 +103,10 @@ module Geode::Commands
         system_exit
       end
 
-      run_script script
+      run_script script, dir
     end
 
-    private def run_script(script : String) : Nil
+    private def run_script(script : String, dir : String) : Nil
       stdout << "» Running script: "
       stdout << '\n' if script.lines.size > 1
       stdout << script.colorize.light_gray << "\n\n"
@@ -88,13 +120,13 @@ module Geode::Commands
           temp = File.tempfile("geode-tmp-run", ".cmd") do |file|
             file << script
           end
-          status = Process.run(temp.path, output: stdout, error: stderr)
+          status = Process.run(temp.path, chdir: dir, output: stdout, error: stderr)
           taken = format_time(Time.monotonic - start)
         ensure
           temp.try &.delete
         end
       {% else %}
-        status = Process.run(script, shell: true, output: stdout, error: stderr)
+        status = Process.run(script, shell: true, chdir: dir, output: stdout, error: stderr)
         taken = format_time(Time.monotonic - start)
       {% end %}
 
