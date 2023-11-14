cf786f3b18f0fee20d33dc48b3d1c20ef628f469
devnote-dev <devnote.dev75@gmail.com>
2023-11-14 01:12:46 +0000

    refactor(commands): try entry-based listing system

--- src/commands/list.cr
+++ src/commands/list.cr
@@ -1,5 +1,23 @@
 module Geode::Commands
   class List < Base
+    private class Entry
+      enum Kind
+        Direct
+        Development
+        Transitive
+        Untracked
+      end
+
+      getter name : String
+      getter version : String
+      property kind : Kind
+
+      def_equals @name, @version
+
+      def initialize(@name, @version, @kind)
+      end
+    end
+
     def setup : Nil
       @name = "list"
       @summary = "lists installed shards"
@@ -20,8 +38,32 @@ module Geode::Commands
       return unless Dir.exists? "lib"
 
       root = Shard.load_local
+      entries = root.dependency_shards.map { |name, spec| Entry.new(name, spec.version, :direct) }
+      entries.concat root.development_shards.map { |name, spec| Entry.new(name, spec.version, :development) }
+
       deps = root.load_dependency_shards
       dev = root.load_development_shards
+      entries = deps.map { |dep| Entry.new(dep.name, dep.version, :direct) }
+      entries.concat dev.map { |dep| Entry.new(dep.name, dep.version, :development) }
+
+      untracked = [] of Entry
+      Dir.each_child("lib") do |child|
+        next if child.starts_with? '.'
+        next unless File.exists?(path = Path["lib", child, "shard.yml"])
+
+        shard = Shard.from_yaml File.read path
+        next if deps.find { |d| d.name == shard.name } || dev.find { |d| d.name == shard.name }
+
+        untracked << Entry.new(shard.name, shard.version, :untracked)
+      end
+
+      unless untracked.empty?
+        resolve_untracked root, entries, untracked
+        entries.concat untracked
+      end
+
+      pp entries
+      return
 
       str = String.build do |io|
         if options.has? "tree"
@@ -60,6 +102,33 @@ module Geode::Commands
       end
 
       stdout.puts str
+    end
+
+    private def resolve_untracked(shard : Shard, resolved : Array(Entry), untracked : Array(Entry)) : Nil
+      deps = shard.load_dependency_shards
+      dev = shard.load_development_shards
+
+      untracked.each do |entry|
+        deps.each do |dep|
+          if dep.dependencies.has_key? name
+            entry.kind = :transitive
+            resolved << entry
+          elsif dep.development.has_key? name
+            entry.kind = :transitive
+            resolved << entry
+          end
+        end
+
+        dev.each do |dep|
+          if dep.dependencies.has_key? name
+            entry.kind = :development
+            resolved << entry
+          elsif dep.development.has_key? name
+            entry.kind = :development
+            resolved << entry
+          end
+        end
+      end
     end
 
     private def format(io : IO, shard : Shard, level : Int32) : Nil
